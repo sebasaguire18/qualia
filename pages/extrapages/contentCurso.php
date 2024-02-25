1cee9cce6577f5e2ed9d6d5b46354e7ddd32a2fd
Sebastián Aguirre Vallejo <sebasavmt@gmail.com>
2024-02-25 01:16:41 -0500

    realizacion de contenido

--- pages/extrapages/contentCurso.php
+++ pages/extrapages/contentCurso.php
@@ -3,20 +3,286 @@
 
     if ($page == 1) {
         ?>
-            <div class="col-12">
-                <p>1. Marco legal</p>
+            <div class="col-12 col-md-9 col-lg-9 col-xl-9">
+                <div class="w-100 w-md-75 w-lg-50 text-center">
+                    <img src="images/t1.png" class="img-fluid">
+                </div>
+                <br>
+                <p>De acuerdo con este orden de importancia y según nuestra constitución política, una resolución no puede modificar  un decreto. Eso significa que el <span class="text-green">Decreto 3075 del 97</span> sigue vigente y la <span class="text-orange">Resolución 2674  del 2013</span> lo complementa. ¡En resumen, se <b>DEBEN</b> que cumplir las dos!</p>
+            </div>
+            <div class="col-12 col-md-3 col-lg-3 col-xl-3 text-center">
+                <img src="images/t1.1.png" class="img-fluid">
             </div>
         <?php
     }elseif ($page == 2) {
         ?>
-            <div class="col-12">
-                <p>2. Conceptos</p>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Alimento</h6>
+                <img src="images/t2_.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Alimento Adulterado</h6>
+                <img src="images/t2_1.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Alimento Contaminado</h6>
+                <img src="images/t2_2.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Alimento de riesgo alto</h6>
+                <img src="images/t2_3.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Alimento de riesgo medio</h6>
+                <img src="images/t2_4.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Alimento de riesgo bajo</h6>
+                <img src="images/t2_5.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Alimento fraudulento</h6>
+                <img src="images/t2_6.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Autoridades sanitarias </h6>
+                <img src="images/t2_7.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>BPM</h6>
+                <img src="images/t2_8.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Concepto/permiso sanitario</h6>
+                <img src="images/t2_9.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Limpieza y desinfección</h6>
+                <img src="images/t2_10.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Expendio</h6>
+                <img src="images/t2_11.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Embalaje y envases</h6>
+                <img src="images/t2_12.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Equipo</h6>
+                <img src="images/t2_13.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Fábrica de alimentos</h6>
+                <img src="images/t2_14.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Higiene de alimentos</h6>
+                <img src="images/t2_15.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Inocuidad de alimentos</h6>
+                <img src="images/t2_16.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Lote</h6>
+                <img src="images/t2_17.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Infestación</h6>
+                <img src="images/t2_18.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Contaminación cruzada</h6>
+                <img src="images/t2_19.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>ETA´S</h6>
+                <img src="images/t2_20.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Brote</h6>
+                <img src="images/t2_21.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Manipulador de alimentos</h6>
+                <img src="images/t2_22.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>La persona que lo producen</h6>
+                <img src="images/t2.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>La persona que los tranporta</h6>
+                <img src="images/t2.1.png" class="img-fluid">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3 col-xl-3 text-center">
+                <h6>Sustancia peligrosa</h6>
+                <img src="images/t2_23.png" class="img-fluid">
             </div>
         <?php
     }elseif ($page == 3) {
         ?>
+            <div class="col-12 col-md-9 col-lg-9 col-xl-9">
+                <ul>
+                    <li>Enfermedades de origen alimentario.</li>
+                    <li>Incluidas las intoxicaciones e infecciones.</li>
+                    <li>Se producen por la ingestión accidental, <i><u>incidental o intencional</u></i> de alimentos o agua, contaminados en cantidades suficientes con <i><u>agentes químicos o microbiológicos.</u></i></li>
+                    <li>Debido a la <i class="text-indigo">deficiencia en el proceso de elaboración, manipulación, conservación, transporte, distribución o comercialización</i> de alimentos y agua.</li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-3 col-lg-3 col-xl-3 text-center">
+                <img src="images/t2_20.png" class="img-fluid">
+            </div>
+            <div class="col-12 text-center">
+                <h4 class="text-secondary1">¿Porqué es super importante?</h4>
+            </div>
+            <div class="col-4 col-md-4 col-lg-4 col-xl-4 text-center text-center">
+                <img src="images/t3.png" class="img-fluid w-75">
+                <h6>¿El lavado de utensilios?</h6>
+            </div>
+            <div class="col-4 col-md-4 col-lg-4 col-xl-4 text-center text-center">
+                <img src="images/t3.1.png" class="img-fluid w-75">
+                <h6>¿El lavado de las manos?</h6>
+            </div>
+            <div class="col-4 col-md-4 col-lg-4 col-xl-4 text-center text-center">
+                <img src="images/t3.2.png" class="img-fluid w-75">
+                <h6>¿El lavado de alimentos?</h6>
+            </div>
+            <br>
+            <div class="col-12 text-center">
+                <hr>   
+                <h3 class="text-secondary1 my-4">3.1 Factores que contribuyen a ETA</h3>
+            </div>
+            <div class="col-12 col-md-8 col-lg-8 col-xl-8">
+                <img src="images/t3.1.1.1.png" class="img-fluid">
+            </div>
+            <div class="col-12 col-md-4 col-lg-4 col-xl-4">
+                <img src="images/t3.1.1.2.png" class="img-fluid">
+            </div>
+            <div class="col-12 text-center">
+                <h4 class="text-secondary1 my-4">¿Cuando hay contaminación?</h4>
+            </div>
+            <div class="col-6 text-center text-center">
+                <img src="images/t3.1.1.png" class="img-fluid w-75">
+            </div>
+            <div class="col-6 text-center text-center">
+                <img src="images/t3.1.2.png" class="img-fluid w-75">
+            </div>
+            <div class="col-12 text-center">
+                <p>Cuando los microbios llegan a los alimentos a travéz del aire, los insectos, el polvo, las manos sucias, etc.</p>
+            </div>
+            <br>
+            <div class="col-12 text-center">
+                <hr>   
+                <h3 class="text-secondary1 my-4">3.2 Clases de Microorganismos Biológicos (MO)</h3>
+            </div>
             <div class="col-12">
-                <p>3. ETAs, contaminación y peligros</p>
+                <img src="images/t3.2_.png" class="img-fluid">
+            </div>
+            <div class="col-12">
+                <h4 class="text-secondary1 my-4">¿Cuando hay descomposición?</h4>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4 col-xl-4">
+                <p>Cuando los microbios crecen y se multiplican dentro del alimento cambiando su sabor, olor y color y éste no se puede consumir.</p>
+                <img src="images/t3.2.1.png" class="img-fluid">
+            </div>
+            <div class="col-12 col-md-8 col-lg-8 col-xl-8">
+                <h4 class="text-secondary1 my-4">¿Qué son los microorganismos o microbios, también llamados gérmenes o bichos?</h4>
+                <img src="images/t3.2.2.png" class="img-fluid">
+                <p>Son seres vivos tan pequeños, que para verlos se requiere un microscopio.</p>
+                <p>Más de cien mil bacterias colocadas una a continuación de otra, escasamente miden un centímetro.</p>
+            </div>
+            <div class="col-12 text-center">
+                <h4 class="text-secondary1 my-4">¿Cuáles son los microbios?</h4>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4 col-xl-4 text-center">
+                <img src="images/t3.2.3.png" class="img-fluid w-75">
+                <p>los hongos</p>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4 col-xl-4 text-center">
+                <img src="images/t3.2.4.png" class="img-fluid w-75">
+                <p>las bacterias</p>
+            </div>
+            <div class="col-12 col-md-4 col-lg-4 col-xl-4 text-center">
+                <img src="images/t3.2.5.png" class="img-fluid w-75">
+                <p>las levaduras</p>
+            </div>
+            <br>
+            <div class="col-12 col-md-9 col-lg-9 col-xl-9">
+                <table class="table table-bordered">
+                    <tr class="text-center">
+                        <th>TIEMPO EN HORAS</th>
+                        <th>NÚMERO DE BACTERIAS</th>
+                    </tr>
+                    <tr class="text-center table-primary">
+                        <th>0</th>
+                        <th>1</th>
+                    </tr>
+                    <tr class="text-center table-primary">
+                        <th>1</th>
+                        <th>8</th>
+                    </tr>
+                    <tr class="text-center table-primary">
+                        <th>2</th>
+                        <th>32</th>
+                    </tr>
+                    <tr class="text-center table-primary">
+                        <th>3</th>
+                        <th>256</th>
+                    </tr>
+                    <tr class="text-center table-primary">
+                        <th>4</th>
+                        <th>2.048</th>
+                    </tr>
+                    <tr class="text-center table-primary">
+                        <th>5</th>
+                        <th>16.384</th>
+                    </tr>
+                    <tr class="text-center table-primary">
+                        <th>6</th>
+                        <th>131.072</th>
+                    </tr>
+                    <tr class="text-center table-primary">
+                        <th>7</th>
+                        <th>1.048.576</th>
+                    </tr>
+                    <tr class="text-center table-primary">
+                        <th>8</th>
+                        <th>16.777.216</th>
+                    </tr>
+                    <tr class="text-center table-primary">
+                        <th>9</th>
+                        <th>134.217.728</th>
+                    </tr>
+                    <tr class="text-center table-primary">
+                        <th>10</th>
+                        <th>1.073.741.824</th>
+                    </tr>
+                </table>
+            </div>
+            <div class="col-12 col-md-3 col-lg-3 col-xl-3 text-center">
+                <p>La temperatura es un factor que ayuda al crecimiento de los microorganismos. A medida que la T° disminuye, la mayoría de los MO reducen la velocidad a la que se reproducen.</p>
+            </div>
+            <br>
+            <div class="col-12 text-center">
+                <hr>
+                <h4 class="text-secondary1 my-4">T° para el desarrollo y control de los MO</h4>
+            </div>
+            <div class="col-12 col-md-8 col-lg-8 col-xl-8 text-center">
+                <img src="images/t3t.png" class="img-fluid">
+            </div>
+            <div class="col-12 col-md-4 col-lg-4 col-xl-4 text-center">
+                <img src="images/t3t1.png" class="img-fluid w-75">
+            </div>
+            <div class="col-12 text-center">
+                <img src="images/3tb.png" class="img-fluid">
+            </div>
+            <div class="col-12 text-center">
+                <img src="images/3tb1.png" class="img-fluid">
+            </div>
+            <div class="col-12 text-center mt-4">
+                <img src="images/3tbp.png" class="img-fluid">
             </div>
         <?php
     }elseif ($page == 4) {
